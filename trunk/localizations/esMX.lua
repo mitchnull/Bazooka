649c3d0f52bc3dadeec96d5061c65622f7ad118f
mitch0 <mitch0@3a069055-1c2b-4764-88cf-4ce3410ff6a8>
2009-09-06 17:43:00 +0000

    initial commit, just a skeleton

---
+++ trunk/localizations/esMX.lua
@@ -0,0 +1,8 @@
+-- This file is automagically generated.
+-- Please visit http://www.wowace.com/projects/range-display/localization/
+-- if you wish to help with the translation.
+
+local L = LibStub("AceLocale-3.0"):NewLocale("Bazooka", "esMX")
+if not L then return end
+
+--@localization(locale="esMX", format="lua_additive_table", handle-subnamespaces="none", namespace="", handle-unlocalized="ignore")@
