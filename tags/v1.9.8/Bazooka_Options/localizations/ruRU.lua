6ca3ee1aee2527ab30b55d450c444e37f51cf9c7
mitch0 <mitch0@3a069055-1c2b-4764-88cf-4ce3410ff6a8>
2010-12-19 13:31:13 +0000

    Tagging as v1.9.8 - better fix for short fade animations: if the anim is too short and no delay is needed, then no animation is created

---
+++ tags/v1.9.8/Bazooka_Options/localizations/ruRU.lua
@@ -0,0 +1,8 @@
+-- This file is automagically generated.
+-- Please visit http://www.wowace.com/projects/bazooka/localization/
+-- if you wish to help with the translation.
+
+local L = LibStub("AceLocale-3.0"):NewLocale("Bazooka_Options", "ruRU")
+if not L then return end
+
+--@localization(locale="ruRU", format="lua_additive_table", handle-subnamespaces="none", namespace="opt", handle-unlocalized="ignore")@
