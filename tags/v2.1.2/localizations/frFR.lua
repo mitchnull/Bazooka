9fab452e5f4ce0ad37b422b67dae00c3d060e75d
mitch0 <mitch0@3a069055-1c2b-4764-88cf-4ce3410ff6a8>
2011-04-27 18:00:44 +0000

    Tagging as v2.1.2

---
+++ tags/v2.1.2/localizations/frFR.lua
@@ -0,0 +1,8 @@
+-- This file is automagically generated.
+-- Please visit http://www.wowace.com/projects/bazooka/localization/
+-- if you wish to help with the translation.
+
+local L = LibStub("AceLocale-3.0"):NewLocale("Bazooka", "frFR")
+if not L then return end
+
+--@localization(locale="frFR", format="lua_additive_table", handle-subnamespaces="none", namespace="", handle-unlocalized="ignore")@
