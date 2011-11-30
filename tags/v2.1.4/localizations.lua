1f8efb9155587cd063727aa1e46eecac2d22f8a0
mitch0 <mitch0@3a069055-1c2b-4764-88cf-4ce3410ff6a8>
2011-11-30 12:12:09 +0000

    Tagging as v2.1.4

---
+++ tags/v2.1.4/localizations.lua
@@ -0,0 +1,61 @@
+local AppName = ...
+local AL = LibStub("AceLocale-3.0")
+local L = AL:NewLocale(AppName, "enUS", true)
+
+L["|cffeda55fDrag|r to move the frame"] = "|cffeda55fDrag|r to move the frame"
+L["|cffeda55fLeft Click|r to lock/unlock frames"] = "|cffeda55fLeft Click|r to lock/unlock frames"
+L["|cffeda55fRight Click|r to open the configuration window"] = "|cffeda55fRight Click|r to open the configuration window"
+L["Disable %s plugin?"] = "Disable %s plugin?"
+L["Bar"] = "Bar"
+L["Bar#%d"] = "Bar#%d"
+L["left"] = "Left"
+L["cleft"] = "Center-Left"
+L["center"] = "Center"
+L["cright"] = "Center-Right"
+L["right"] = "Right"
+L["top"] = "Top"
+L["bottom"] = "Bottom"
+L["none"] = "None"
+
+-----------------------------------------------------------------------------
+
+L = AL:NewLocale(AppName, "deDE")
+if L then
+--@localization(locale="deDE", format="lua_additive_table", handle-subnamespaces="none", namespace="", handle-unlocalized="ignore")@
+end
+
+L = AL:NewLocale(AppName, "esES")
+if L then
+--@localization(locale="esES", format="lua_additive_table", handle-subnamespaces="none", namespace="", handle-unlocalized="ignore")@
+end
+
+L = AL:NewLocale(AppName, "esMX")
+if L then
+--@localization(locale="esMX", format="lua_additive_table", handle-subnamespaces="none", namespace="", handle-unlocalized="ignore")@
+end
+
+L = AL:NewLocale(AppName, "frFR")
+if L then
+--@localization(locale="frFR", format="lua_additive_table", handle-subnamespaces="none", namespace="", handle-unlocalized="ignore")@
+end
+
+L = AL:NewLocale(AppName, "koKR")
+if L then
+--@localization(locale="koKR", format="lua_additive_table", handle-subnamespaces="none", namespace="", handle-unlocalized="ignore")@
+end
+
+L = AL:NewLocale(AppName, "ruRU")
+if L then
+--@localization(locale="ruRU", format="lua_additive_table", handle-subnamespaces="none", namespace="", handle-unlocalized="ignore")@
+end
+
+L = AL:NewLocale(AppName, "zhCN")
+if L then
+--@localization(locale="zhCN", format="lua_additive_table", handle-subnamespaces="none", namespace="", handle-unlocalized="ignore")@
+end
+
+L = AL:NewLocale(AppName, "zhTW")
+if L then
+--@localization(locale="zhTW", format="lua_additive_table", handle-subnamespaces="none", namespace="", handle-unlocalized="ignore")@
+end
+
