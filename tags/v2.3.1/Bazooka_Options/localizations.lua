4bcbc4654f8e6b0d8f8eb1150a18db31964f3a6d
mitch0 <mitch0@3a069055-1c2b-4764-88cf-4ce3410ff6a8>
2014-01-29 12:26:02 +0000

    Tagging as v2.3.1

---
+++ tags/v2.3.1/Bazooka_Options/localizations.lua
@@ -0,0 +1,160 @@
+local AppName = ...
+local AL = LibStub("AceLocale-3.0")
+local L = AL:NewLocale(AppName, "enUS", true)
+
+L["Adjust frames"] = "Adjust frames"
+L["Alignment"] = "Alignment"
+L["Apply"] = "Apply"
+L["Apply selected options to selected bars?"] = "Apply selected options to selected bars?"
+L["Apply selected options to selected plugins?"] = "Apply selected options to selected plugins?"
+L["Area"] = "Area"
+L["Attach point"] = "Attach point"
+L["Auto apply"] = "Auto apply"
+L["Background"] = "Background"
+L["Background color"] = "Background color"
+L["Background settings"] = "Background settings"
+L["Background texture"] = "Background texture"
+L["Background tile size"] = "Background tile size"
+L["Bar"] = "Bar"
+L["Bars"] = "Bars"
+L["Border color"] = "Border color"
+L["Border inset"] = "Border inset"
+L["Border texture"] = "Border texture"
+L["Border thickness"] = "Border thickness"
+L["Bottom"] = "Bottom"
+L["Bulk Configuration"] = "Bulk Configuration"
+L["Center spacing"] = "Center spacing"
+L["Clear"] = "Clear"
+L["Create new bar"] = "Create new bar"
+L["Disable during pet battle"] = "Disable during pet battle"
+L["Disable mouse in combat"] = "Disable mouse in combat"
+L["Disable mouse out of combat"] = "Disable mouse out of combat"
+L["Disable minimap icons"] = "Disable minimap icons"
+L["Disable tooltip"] = "Disable tooltip"
+L["Disable tooltip in combat"] = "Disable tooltip in combat"
+L["Enable background"] = "Enable background"
+L["Enable opacity workaround"] = "Enable opacity workaround"
+L["Enabled"] = "Enabled"
+L["Enable highlight"] = "Enable highlight"
+L["Enable simple tooltips"] = "Enable simple tooltips"
+L["Fade in combat"] = "Fade in combat"
+L["Fade-in duration"] = "Fade-in duration"
+L["Fade opacity"] = "Fade opacity"
+L["Fade-out delay"] = "Fade-out delay"
+L["Fade-out duration"] = "Fade-out duration"
+L["Fade out of combat"] = "Fade out of combat"
+L["Fit to content width"] = "Fit to content width"
+L["Font"] = "Font"
+L["Font outline"] = "Font outline"
+L["Font size"] = "Font size"
+L["Force Hide Tooltip"] = "Force hide tooltip"
+L["Force hiding the tooltip. It's a hack and might cause unwanted side-effects. Only enable if the plugin's tooltip doesn't behave as expected."] = "Force hiding the tooltip. It's a hack and might cause unwanted side-effects. Only enable if the plugin's tooltip doesn't behave as expected."
+L["Frame strata"] = "Frame strata"
+L["Height"] = "Height"
+L["Hide tooltip on click"] = "Hide tooltip on click"
+L["High"] = "High"
+L["Icon Border Clip"] = "Icon border clip"
+L["Icon size"] = "Icon size"
+L["Icon-text spacing"] = "Icon-text spacing"
+L["Label color"] = "Label color"
+L["Left"] = "Left"
+L["Left margin"] = "Left margin"
+L["Left spacing"] = "Left spacing"
+L["Locked"] = "Locked"
+L["Low"] = "Low"
+L["Manual tooltip"] = "Manual tooltip"
+L["Manual tooltip toggle mode"] = "Manual tooltip toggle mode"
+L["Max text width"] = "Max text width"
+L["Medium"] = "Medium"
+L["None"] = "None"
+L["Normal"] = "Normal"
+L["Opacity"] = "Opacity"
+L["Override Tooltip Scale"] = "Override tooltip scale"
+L["Plugin display settings"] = "Plugin display settings"
+L["Plugins"] = "Plugins"
+L["Remove bar"] = "Remove bar"
+L["Remove %s?"] = "Remove %s?"
+L["Right"] = "Right"
+L["Right margin"] = "Right margin"
+L["Right spacing"] = "Right spacing"
+L["Select All"] = "Select All"
+L["Selection"] = "Selection"
+L["Set if you want to hide the plugin tooltip when releasing the modifier key in manual tooltip mode"] = "Set if you want to hide the plugin tooltip when releasing the modifier key in manual tooltip mode"
+L["Settings"] = "Settings"
+L["Show disabled plugins last"] = "Show disabled plugins last"
+L["Show icon"] = "Show icon"
+L["Show label"] = "Show label"
+L["Show suffix"] = "Show suffix"
+L["Show text"] = "Show text"
+L["Show title"] = "Show title"
+L["Show value"] = "Show value"
+L["Shrink threshold"] = "Shrink threshold"
+L["Show tooltip by pressing a modifier key"] = "Show tooltip by pressing a modifier key"
+L["Side spacing"] = "Side spacing"
+L["Strata"] = "Strata"
+L["Strip colors"] = "Strip colors"
+L["Statusbar"] = "Statusbar"
+L["Suffix color"] = "Suffix color"
+L["Text color"] = "Text color"
+L["Texture type"] = "Texture type"
+L["Tooltip Scale"] = "Tooltip scale"
+L["The size used to tile the background texture"] = "The size used to tile the background texture"
+L["Thick"] = "Thick"
+L["Tile background"] = "Tile background"
+L["Top"] = "Top"
+L["Tweak anchor positions"] = "Tweak anchor positions"
+L["Use label as title"] = "Use label as title"
+L["Width"] = "Width"
+
+-----------------------------------------------------------------------------
+
+L = AL:NewLocale(AppName, "deDE")
+if L then
+--@localization(locale="deDE", format="lua_additive_table", handle-subnamespaces="none", namespace="opt", handle-unlocalized="ignore")@
+end
+
+L = AL:NewLocale(AppName, "esES")
+if L then
+--@localization(locale="esES", format="lua_additive_table", handle-subnamespaces="none", namespace="opt", handle-unlocalized="ignore")@
+end
+
+L = AL:NewLocale(AppName, "esMX")
+if L then
+--@localization(locale="esMX", format="lua_additive_table", handle-subnamespaces="none", namespace="opt", handle-unlocalized="ignore")@
+end
+
+L = AL:NewLocale(AppName, "frFR")
+if L then
+--@localization(locale="frFR", format="lua_additive_table", handle-subnamespaces="none", namespace="opt", handle-unlocalized="ignore")@
+end
+
+L = AL:NewLocale(AppName, "koKR")
+if L then
+--@localization(locale="koKR", format="lua_additive_table", handle-subnamespaces="none", namespace="opt", handle-unlocalized="ignore")@
+end
+
+L = AL:NewLocale(AppName, "ruRU")
+if L then
+--@localization(locale="ruRU", format="lua_additive_table", handle-subnamespaces="none", namespace="opt", handle-unlocalized="ignore")@
+end
+
+L = AL:NewLocale(AppName, "zhCN")
+if L then
+--@localization(locale="zhCN", format="lua_additive_table", handle-subnamespaces="none", namespace="opt", handle-unlocalized="ignore")@
+end
+
+L = AL:NewLocale(AppName, "zhTW")
+if L then
+--@localization(locale="zhTW", format="lua_additive_table", handle-subnamespaces="none", namespace="opt", handle-unlocalized="ignore")@
+end
+
+L = AL:NewLocale(AppName, "ptBR")
+if L then
+--@localization(locale="ptBR", format="lua_additive_table", handle-subnamespaces="none", namespace="opt", handle-unlocalized="ignore")@
+end
+
+L = AL:NewLocale(AppName, "itIT")
+if L then
+--@localization(locale="itIT", format="lua_additive_table", handle-subnamespaces="none", namespace="opt", handle-unlocalized="ignore")@
+end
+
