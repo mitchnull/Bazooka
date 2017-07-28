eff68404f3f3e799cb549815f63014d3605098e9
mitch0 <mitch0@3a069055-1c2b-4764-88cf-4ce3410ff6a8>
2017-07-28 13:21:43 +0000

    quick fix for issue#55

--- trunk/Bazooka.lua
+++ trunk/Bazooka.lua
@@ -2434,6 +2434,12 @@ function Bazooka:applySettings()
         return
     end
     self:toggleLocked(self.db.profile.locked == true)
+    for i = 1, #self.bars do
+        local bar = self.bars[i]
+        if bar.db.marked and bar.db.hidden then
+            bar:disableAndHide(true)
+        end
+    end
     if self.db.profile.disableDBIcon then
         self:disableDBIcon()
     else
